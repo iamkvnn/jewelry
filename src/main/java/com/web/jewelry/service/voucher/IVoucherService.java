c709c751e2baf80c5f9fd225eeffc4d192ab1641
iamkvnn <23110119@studnet.hcmute.edu.vn>
2025-05-04 01:31:28 +0700

    refactor voucher

--- src/main/java/com/web/jewelry/service/voucher/IVoucherService.java
+++ src/main/java/com/web/jewelry/service/voucher/IVoucherService.java
@@ -3,18 +3,19 @@ package com.web.jewelry.service.voucher;
 import com.web.jewelry.dto.request.OrderRequest;
 import com.web.jewelry.dto.request.VoucherRequest;
 import com.web.jewelry.dto.response.VoucherResponse;
-import com.web.jewelry.enums.EVoucherType;
 import com.web.jewelry.model.Voucher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 
 public interface IVoucherService {
-    List<Voucher> getAllVouchers();
-    List<Voucher> searchVouchers(String query);
+    Page<Voucher> getAllVouchers(Pageable pageable);
+    Page<Voucher> searchVouchers(String query, Pageable pageable);
     Voucher getVoucherById(Long id);
     Voucher getVoucherByCode(String code);
-    List<Voucher> getValidVouchers();
+    Page<Voucher> getValidVouchers(Pageable pageable);
     Voucher addVoucher(VoucherRequest request);
     Voucher updateVoucher(Long id, VoucherRequest request);
     void deleteVoucher(Long id);
@@ -23,9 +24,9 @@ public interface IVoucherService {
 
     List<Voucher> validateVouchers(OrderRequest request);
     VoucherResponse convertToResponse(Voucher voucher);
-    List<VoucherResponse> convertToResponse(List<Voucher> vouchers);
+    Page<VoucherResponse> convertToResponse(Page<Voucher> vouchers);
 
-    List<Voucher> getValidVouchersForOrder(OrderRequest request);
+    Page<Voucher> getValidVouchersForOrder(OrderRequest request, Pageable pageable);
 
     void decreaseVoucherQuantity(Long id);
 }
